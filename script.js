fe1e59ff94b683fcd496f7e139008783b1697c25
Diana Bali <dianabali64@gmail.com>
2025-09-27 21:14:17 +0200

    Initial commit

--- script.js
+++ script.js
@@ -19,14 +19,49 @@ const cardsEl = [];
 // Store card data
 const cardsData = [
     { question: 'What is JavaScript?',
-      answer: 'JavaScript is a programming language that is commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites. Over the years, JavaScript has evolved into a versatile language that can be used for both client-side and server-side development.'
+      answer: 'JavaScript is a programming language that is commonly used in web development.'
     },
     { question: 'What is a closure?',
-      answer: 'A closure is a feature in JavaScript where an inner function has access to the outer (enclosing) function\'s variables and parameters, even after the outer function has returned. This allows the inner function to "remember" the environment in which it was created, enabling data encapsulation and the creation of private variables.'
+      answer: 'A closure is a feature in JavaScript where an inner function has access to the outer (enclosing) function\'s variables and parameters, even after the outer function has returned.'
     },
     { question: 'What is HTML?',
-      answer: 'HTML (HyperText Markup Language) is the standard markup language used to create and structure content on the web. It provides the basic building blocks for web pages, allowing developers to define elements such as headings, paragraphs, links, images, and more.'
+      answer: 'HTML (HyperText Markup Language) is the standard markup language used to create and structure content on the web.'
     }
 ];
 
+// Create cards
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+}
 
+// Create a single card
+function createCard(data, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // Set the first card to be active
+    if(index === 0) {
+        card.classList.add('active');
+    }
+
+    // Add inner HTML
+    card.innerHTML = `
+        <div class="inner-card">
+            <div class="inner-card-front">
+                <p>${data.question}</p>
+            </div>
+            <div class="inner-card-back">
+                <p>${data.answer}</p>
+            </div>
+        </div>
+    `;
+
+    // Add to DOM cards
+    cardsEl.push(card);
+
+    // Append to container
+    cardsContainer.appendChild(card);
+}
+
+// Show cards
+createCards();
